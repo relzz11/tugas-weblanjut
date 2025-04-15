b2fcb3e8961f9ecfc778e7ab629276185248f427
relzz11 <seventen909@gmail.com>
2025-04-15 15:14:14 +0700

    selesai

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     private $products = [
-        1 => ['name' => 'Kaos Laravel', 'price' => 150000, 'jenis'=> 'pakaian','merek' => 'inkalum', 'material' => 'kain'],
-        2 => ['name' => 'Stiker Coding', 'price' => 250000, 'jenis' => 'pakaian', 'merek' => 'inkalum', 'material' => 'kertas'],
-        3 => ['name' => 'NoteBook Dev', 'price' => 50000,'jenis' => 'pakaian', 'merek' => 'inkalum', 'material' => 'kerastul'], 
+        1 => ['name' => 'Kaos Laravel', 'price' => 150000, 'jenis'=> 'barang','merek' => 'olegan', 'material' => 'kalum'],
+        2 => ['name' => 'Stiker Coding', 'price' => 250000, 'jenis' => 'barang', 'merek' => 'olegan', 'material' => 'katuban'],
+        3 => ['name' => 'NoteBook Dev', 'price' => 50000,'jenis' => 'barang', 'merek' => 'olegan', 'material' => 'kerastul'], 
     ];
 
     // menampilkan daftar produk
